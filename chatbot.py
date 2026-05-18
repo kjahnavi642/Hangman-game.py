fdf35ae4903f25a0a6c427125b19f474fd0a79ee
kjahnavi642 <kjahnavi642@gmail.com>
2026-05-18 09:48:03 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+word=input ("enter a value:")
+print(word)
+msg=input("enter message:")
+if msg=="hello":
+  print("hi!how are you?")
+elif msg=="bye":
+   print ("good bye!")
+else:
+    print("I don't understand ")
+def greet():
+   print ("Good Morning ")
+greet()
+for i in range(5):
+ print (i)
+ i=1
+while i<=5:
+   print(i)
+   i=i+1
